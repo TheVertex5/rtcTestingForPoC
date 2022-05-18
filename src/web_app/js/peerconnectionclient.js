7d039a2d7b25b2b2d7ea452d7d263a4d7de37383
TheVertex5 <5er.znidar@gmail.com>
2022-05-18 14:09:09 +0000

    ..

--- src/web_app/js/peerconnectionclient.js
+++ src/web_app/js/peerconnectionclient.js
@@ -257,7 +257,7 @@ PeerConnectionClient.prototype.onSetRemoteDescriptionSuccess_ = function() {
 };
 
 PeerConnectionClient.prototype.processSignalingMessage_ = function(message) {
-  trace("#P; message_type: "+message.type+" in: "+message);
+  trace("#P; message_type: "+message.type+" in: "+JSON.stringify(message));
   if (message.type === 'offer' && !this.isInitiator_) {
     if (this.pc_.signalingState !== 'stable') {
       trace('ERROR: remote offer received in unexpected state: ' +
